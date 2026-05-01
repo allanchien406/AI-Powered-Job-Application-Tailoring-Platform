20a3af8b4b8601c10fb3987263c6b2da609e1b7b
Allan Chien <100819436+AllanChien@users.noreply.github.com>
2026-05-01 14:02:07 +1200

    Split job description service into separate Lambda

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -123,7 +123,30 @@ export class InfraStack extends cdk.Stack {
       },
     });
 
+    const jobDescriptionServiceHandler = new Function(
+      this,
+      "JobDescriptionServiceHandler",
+      {
+        runtime: Runtime.PYTHON_3_12,
+        handler: "index.handler",
+        code: Code.fromAsset("lambda/job-service"),
+        vpc,
+        vpcSubnets: {
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+        timeout: cdk.Duration.seconds(10),
+        securityGroups: [lambdaSecurityGroup],
+        environment: {
+          DB_HOST: rdsInstance.dbInstanceEndpointAddress,
+          DB_PORT: rdsInstance.dbInstanceEndpointPort,
+          DB_NAME: "jobtailor",
+          DB_SECRET_ARN: rdsInstance.secret?.secretArn || "",
+        },
+      },
+    );
+
     rdsInstance.secret?.grantRead(profileServiceHandler); // Grant the Lambda function permission to read the RDS secret for database credentials
+    rdsInstance.secret?.grantRead(jobDescriptionServiceHandler); // Grant the Lambda function permission to read the RDS secret for database credentials
 
     // Create the HTTP API Gateway and integrate it with the Lambda function
     const api = new HttpApi(this, "ProfileServiceApi", {
@@ -155,7 +178,7 @@ export class InfraStack extends cdk.Stack {
       ],
       integration: new HttpLambdaIntegration(
         "JobDescriptionHandlerIntegration",
-        profileServiceHandler,
+        jobDescriptionServiceHandler,
       ),
     });
 
